eaee809c33f909ca1d02c709e737cf4542107ded
Franklin-Qi <18513412153@163.com>
2020-07-17 16:37:20 +0800

    add some files to delete in clear.sh

--- clear.sh
+++ clear.sh
@@ -6,7 +6,10 @@ rm -f kylin-scanner.pro.user \
     .qmake.stash \
     Makefile \
     src/Makefile \
-    src/kylin-scanner
+    src/kylin-scanner \
+    debian/debhelper-build-stamp \
+    debian/files \
+    debian/kylin-scanner.substvars
 
 rm -rf debian/.debhelper/ \
     debian/kylin-scanner
